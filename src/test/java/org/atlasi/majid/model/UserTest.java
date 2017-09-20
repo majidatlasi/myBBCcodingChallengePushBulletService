730682c5880e7d27c32f4dfa521aaad08150ce30
Majid Atlasi <majid.atlasi@travisperkins.co.uk>
2017-09-20 17:11:02 +0100

    Added User Test

---
+++ src/test/java/org/atlasi/majid/model/UserTest.java
@@ -0,0 +1,58 @@
+package org.atlasi.majid.model;
+
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserTest {
+
+	private static final String userName = "myName";
+	private static final String accessToken = "XYZ";
+	private User user;
+
+	@Before
+	public void setUp() throws Exception {
+		user = new User(userName, accessToken);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetUserName() {
+		Assert.assertEquals(userName, user.getUserName());
+
+	}
+
+	@Test
+	public void testGetAccessToken() {
+		Assert.assertEquals(accessToken, user.getAccessToken());
+	}
+
+	@Test
+	public void testGetNumOfNotificationsPushed() {
+		Assert.assertEquals(0, user.getNumOfNotificationsPushed());
+	}
+
+	@Test
+	public void testIncrementNumOfNotificationsPushed() {
+		user.incrementNumOfNotificationsPushed();
+		user.incrementNumOfNotificationsPushed();
+		Assert.assertEquals(2, user.getNumOfNotificationsPushed());
+	}
+
+	@Test
+	public void testGetCreationDate() {
+		SimpleDateFormat sdf = new SimpleDateFormat("EEE, MMM d, ''yy");
+		
+		assertEquals(sdf.format(new Date()), sdf.format(user.getCreationDate()));
+	}
+
+}
