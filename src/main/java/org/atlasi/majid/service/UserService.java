64f8154d604af96deec54d5b3a2a24948d4a32fb
Majid Atlasi <majid.atlasi@travisperkins.co.uk>
2017-09-20 13:29:34 +0100

    Added UserService interface

--- src/main/java/org/atlasi/majid/service/UserService.java
+++ src/main/java/org/atlasi/majid/service/UserService.java
@@ -1,5 +1,18 @@
 package org.atlasi.majid.service;
 
+import java.util.List;
+
+import org.atlasi.majid.model.User;
+
 public interface UserService {
 
+	User findByName(String name);
+
+	void saveUser(User user);
+
+	List<User> findAllUsers();
+
+	boolean userExists(User user);
+
+	void incrementNumOfNotificationsPushed(User user);
 }
