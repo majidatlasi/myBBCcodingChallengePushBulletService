67cba903d695db75b92a5a5d875169e027d76b1c
Majid Atlasi <majid.atlasi@travisperkins.co.uk>
2017-09-20 13:03:28 +0100

    First commit

---
+++ src/main/java/org/atlasi/majid/MyBbcPushBulletServiceApplication.java
@@ -0,0 +1,12 @@
+package org.atlasi.majid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBbcPushBulletServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBbcPushBulletServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/org/atlasi/majid/MyBbcPushBulletServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.atlasi.majid;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyBbcPushBulletServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
